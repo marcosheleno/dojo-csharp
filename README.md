44aec16eb5c2820724227263c990e62e22a9e925
luanlsr <luanfswd@gmail.com>
2022-02-15 10:04:44 -0300

    dojo-luan

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Teremos dois dominios nessa aplicação:
 Os contatos são entidade compostas por id e nome(brigatóriamente) e podem pertencer a grupos de contatos, ter muitos telefones e emails;
 Já salas de mensagem são compostas por id, nome e 1 contato que receberá as mensagens. Para cada mensagem preciso saber seu conteúdo e a data/hora do envio.
 
-
+d
 Lembre-se dos **principios SOLID** e que tudo deve ser **testável** e **indepentedente**
           
 Para hoje vamos precisar de:
--- src/Dojo/Contact.Domain/Contact.cs
+++ src/Dojo/Contact.Domain/Contact.cs
@@ -2,9 +2,15 @@ namespace Contact.Domain
 {
     public class Contact : IContact
     {
+        
         int IContact.ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         string IContact.Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         string IContact.Emails { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         int IContact.Telefones { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        static void IContact() {
+            
+            
+        }
     }
 }
