d2be2146092cf744fec6d3d9e3dc1b7e86a44147
Jean Santos Silva <40009808+jean07ss@users.noreply.github.com>
2022-02-15 10:37:51 -0300

    Atualizacao Estrutura

--- src/Dojo/Contact.Domain/Contact.cs
+++ src/Dojo/Contact.Domain/Contact.cs
@@ -1,13 +1,17 @@
+using static Contact.Domain.Email.Entity;
 namespace Contact.Domain
 {
     class Contact : BaseClass
     {
         private string name;
 
+        private Entity email;
+
         public Contact(Guid id, string name)
         {
             this.id = id;
             this.name = name;
+            this.Email = new Entity();
         }
     }
 }
--- src/Dojo/Contact.Domain/Group/Entity.cs
+++ src/Dojo/Contact.Domain/Group/Entity.cs
@@ -1,1 +1,12 @@
 namespace Contact.Domain.Group
+{
+    class Entity : BaseClass
+    {
+        private string groupName;
+
+        public Entity(string groupName)
+        {
+            this.groupName = groupName;
+        }
+    }
+}
